07b78e5bf9e0426ffbd4cb8ca733a0266c32f5b6
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-01 13:42:21 -0500

    pass a number (value property) to the Key component that uses this input to create a button

--- src/app/components/key/key.component.ts
+++ src/app/components/key/key.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-key',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./key.component.css']
 })
 export class KeyComponent implements OnInit {
+  @Input() value: number;
 
   constructor() { }
 
