03e253244db02938bbe9845ab53ad00166f19ed0
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-04 21:39:58 -0500

    implement the operator component and make him be a button that takes the value of what is being passed to it

--- src/app/components/operator/operator.component.ts
+++ src/app/components/operator/operator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-operator',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./operator.component.css']
 })
 export class OperatorComponent implements OnInit {
+  @Input() value: any;
 
   constructor() { }
 
