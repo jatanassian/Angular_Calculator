4e0fb3aad18b820245567bcbe3610b96223b97bb
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-18 15:48:53 -0500

    place all the buttons on the calculator

--- src/app/components/operator/operator.component.ts
+++ src/app/components/operator/operator.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-operator',
   templateUrl: './operator.component.html',
-  styleUrls: ['./operator.component.scss']
+  styleUrls: ['./operator.component.scss'],
 })
 export class OperatorComponent implements OnInit {
   @Input() value: string;
